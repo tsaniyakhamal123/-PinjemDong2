949979bd90ed930d2271e236f0a072ed2975379d
nasywarps <nasywarahmadhaniputrisetiawan@mail.ugm.ac.id>
2024-11-16 14:26:18 +0700

    feat: add class files from the previous repo

---
+++ homepageJUnpro/OrderCheckout.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinjemDong
+{
+    public class OrderCheckout
+    {
+        private int _barangID;
+        public string _namaPenerima;
+        public string _alamat;
+        public string _payMethod;
+        public const int biayaLayanan = 5000;
+        public const int ongkir = 10000;
+        public int _jumlahBarang;
+        private int _hargaBarang;
+        public int _subtotal;
+        public int _total;
+
+        public int Subtotal { get
+            {
+                return _subtotal;
+            }
+
+            set
+            {
+                _subtotal = _jumlahBarang * _hargaBarang;
+            }
+        }
+
+        public OrderCheckout(int jumlahBarang)
+        {
+            _jumlahBarang = jumlahBarang;
+        }
+
+        public void HitungHarga()
+        {
+            _total = 0;
+            _total = Subtotal + ongkir + biayaLayanan;
+        }
+    }
+}
